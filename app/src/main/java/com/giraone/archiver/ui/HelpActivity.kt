11dbef68b97a0404c7f7311ee94bf60972ab4870
Hans Schuell <hans.schuell@gmx.de>
2025-09-18 23:00:18 +0200

    Import fixed

--- app/src/main/java/com/giraone/archiver/ui/HelpActivity.kt
+++ app/src/main/java/com/giraone/archiver/ui/HelpActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
